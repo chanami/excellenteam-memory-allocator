cd787089181edba86c77219d6ab7d4cc9015c6e4
chanami <chanami15@gmail.com>
2018-11-23 02:48:14 -0800

    init first block in init memory allocator

--- memory_allocator.c
+++ memory_allocator.c
@@ -14,14 +14,13 @@ MemoryAllocator* MemoryAllocator_init(void* memoryPool, size_t size){
 
     ma->m_size=size;
     ma->m_memory_pointer=memoryPool;
-    *((size_t*)ma->m_memory_pointer)=0
+    *((size_t*)ma->m_memory_pointer)=0;
 
     return ma;
 }
 
 void* MemoryAllocator_allocate(MemoryAllocator* allocator, size_t size){
     int i;
-    size_t m_size=allocator->m_size;
 
     for(i=0; i <allocator->m_size; i++){
 
