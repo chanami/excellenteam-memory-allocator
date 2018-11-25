0602098366e8f183bdfcbef086913504d489f1f6
chanami <chanami15@gmail.com>
2018-11-25 03:45:11 -0800

    allocate function

--- memory_allocator.c
+++ memory_allocator.c
@@ -1,12 +1,16 @@
 #include "memory_allocator.h"
-
+#define AVAILABLE 0
+#define OCCUPIED 1
+#define MANAGER sizeof(size_t)
 
 struct MemoryAllocator{
     size_t m_size;
     void *m_memory_ptr;
 };
 
-MemoryAllocator* MemoryAllocator_init(void* memoryPool, size_t size){
+MemoryAllocator* MemoryAllocator_init(void* memoryPool, size_t size)
+{
+    
     MemoryAllocator * ma = malloc(sizeof(MemoryAllocator));
 
     if(size%sizeof(size_t)==0)
@@ -14,18 +18,47 @@ MemoryAllocator* MemoryAllocator_init(void* memoryPool, size_t size){
 
     ma->m_size=size;
     ma->m_memory_ptr=memoryPool;
-    *((size_t*)ma->m_memory_ptr)=size;
-    *((size_t*)ma->m_memory_ptr)|= 1 << 0;
+    *((size_t*)ma->m_memory_ptr)=size-MANAGER;
 
     return ma;
 }
 
-void* MemoryAllocator_allocate(MemoryAllocator* allocator, size_t size){
-    int i;
+void* MemoryAllocator_allocate(MemoryAllocator* allocator, size_t size)
+{
+    /* iterates on block managers */
+    void* current_block = allocator->m_memory_ptr;
+    /*size of the current block*/
+    size_t index;
+    size_t *end_of_allocator = (size_t*)current_block + allocator->m_size;
 
-    for(i=0; i <allocator->m_size; i++){
+    /*align size to sizeof size_t */
+    while(size++ % sizeof(size_t)){}
 
+    while(current_block != end_of_allocator)
+    {
+        /*if current block is available*/
+        if((*((size_t*)current_block) & AVAILABLE)==AVAILABLE)
+        {
+            /*if current block size fits*/
+            if(*((size_t*)current_block - AVAILABLE) >= size)
+            {
+                if(*((size_t*)current_block - AVAILABLE) > size )
+                    *(((size_t*)current_block) + size + MANAGER ) = *((size_t*)current_block) - size - MANAGER;
 
+                *(((size_t*)current_block)) = size + OCCUPIED;
+                return current_block;
+            }
+
+                /*combine with the next block*/
+            else if((*((size_t*)current_block + *((size_t*)current_block) + MANAGER ) & AVAILABLE)==AVAILABLE)
+                *((size_t*)current_block) += *((size_t*)current_block + *((size_t*)current_block) + MANAGER) -AVAILABLE ;
+        }
+        else
+        {
+            /*if not available goes to the next block*/
+            index = *((size_t*)current_block) + MANAGER;
+            current_block = index +(size_t*)current_block ;
+        }
     }
     return NULL;
 }
